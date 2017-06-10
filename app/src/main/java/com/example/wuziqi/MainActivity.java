960c72be163a4d1064681b1ca01a09de25ce2df5
Your Name <you@example.com>
2017-06-10 14:23:02 +0800

    实现五子棋输赢的判断

---
+++ app/src/main/java/com/example/wuziqi/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.wuziqi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WuziqiPanel mWuziqiPanel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mWuziqiPanel = (WuziqiPanel) findViewById(R.id.wuziqi);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_again,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_again){
+            mWuziqiPanel.start();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
